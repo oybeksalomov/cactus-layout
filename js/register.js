ed48fde06e5f01f08bb7c95edbb012962dddd125
Oybek Salomov <oybeksalomov@gmail.com>
2023-01-24 13:31:38 +0500

    edit register page

--- js/register.js
+++ js/register.js
@@ -1,3 +1,45 @@
 "use strict"
 
-// todo:  register two part
+const firstTab = document.querySelector("#firstTab"),
+  firstTabInput = firstTab.querySelectorAll("input"),
+  secondTab = document.querySelector("#secondTab"),
+  secondTabInput = secondTab.querySelectorAll("input"),
+  submitBtn = document.querySelector("#submitBtn"),
+  nextPrevBtn = document.querySelector("#nextPrevBtn"),
+  step = document.querySelectorAll('.step')
+
+submitBtn.addEventListener('click', () => {
+  secondTabInput.forEach(element => {
+    if(element.value == false) {
+      element.classList.add('invalid')
+    } else {
+      element.classList.remove('invalid')
+    }
+  })
+
+})
+nextPrevBtn.addEventListener('click', () => {
+
+  firstTabInput.forEach(element => {
+    if(element.value == false) {
+      element.classList.add('invalid')
+    } else {
+      element.classList.remove('invalid')
+      secondTab.classList.toggle('hideElem')
+      firstTab.classList.toggle('hideElem')
+      submitBtn.classList.toggle('hideElem') 
+      step[0].classList.toggle('activeStep')
+      step[1].classList.toggle('activeStep')
+    }
+  })
+
+  nexPrev()
+})
+
+function nexPrev() {
+  if(firstTab.className !== "tab") {
+    nextPrevBtn.innerHTML = "Orqaga"
+  } else {
+    nextPrevBtn.innerHTML = "Keyingi"
+  }
+}
